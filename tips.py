331ba8e08ce414759f01425e643ade0d231f16ef
roman-ollv <106754286+roman-ollv@users.noreply.github.com>
2022-08-01 17:03:54 +0200

    Update tips.py

--- tips.py
+++ tips.py
@@ -5,7 +5,7 @@ parser.add_argument('-a2','--a_copy', help = ('now lyrics are changed'), type = 
 parser.add_argument('-n', '--name', help='a name to personalize motivational speech', type = input, required=True)
 args =  parser.parse_args()
 
-name = input("Write your name")
+#name = input("Write your name")
 def CLI_function(a, a_copy, name):
     with open (a, 'r') as rhomework:
         with open (a_copy,'w') as whomework:
